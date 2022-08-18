9d226f62528f528ebc4fb2f759b0762965c4947d
TonyBeckley <tonybeckley43@gmail.com>
2022-08-18 18:30:35 +0100

    adjusted the cartDescription div in the mobile view, hamburger menu and navMenu .active not working

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const cartIcon = document.querySelector(".cartIcon");
 const cartDescription = document.querySelector(".cartDescription");
+const hamburger = document.querySelector("hamburger");
+const navMenu = document.querySelector(".navMenu");
 
 cartIcon.addEventListener("click", () => {
     cartDescription.classList.toggle("active");
-})
+});
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
